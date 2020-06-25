b6122366ab4191b824d30a397709d46b2850b83d
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-25 09:24:03 +0100

    readme

--- environment/provision_file.sh
+++ environment/provision_file.sh
@@ -3,9 +3,11 @@
 apt-get -y update
 
 apt-get -y install nginx
-
+# install nginx
 service nginx start
-
+#install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-
-npm install -g pm2
+#install pm2
+sudo npm install pm2 -g
